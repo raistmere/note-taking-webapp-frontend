ceba78dfb85af8be1efa3d818ab7ac0d609f2901
raistmere <raistmere@gmail.com>
2025-01-25 15:10:11 -0600

    Style: added styling to note detail view that allows user to view the note in detail.
    Styling matches figma document (75%) but it's still the first draft. Moving on to editing, deleting, and creating new notes.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,7 +7,9 @@ import searchIcon from "../../assets/images/icon-search.svg";
 import archiveIcon from "../../assets/images/icon-archive.svg";
 import tagIcon from "../../assets/images/icon-tag.svg";
 import settingsIcon from "../../assets/images/icon-settings.svg";
-// import plusIcon from "../../assets/images/icon-plus.svg";
+import leftArrowIcon from "../../assets/images/icon-arrow-left.svg";
+import trashIcon from "../../assets/images/icon-delete.svg";
+import timeIcon from "../../assets/images/icon-clock.svg";
 
 interface Note {
 
@@ -20,6 +22,8 @@ interface Note {
 function Dashboard() {
 
     const [notes, setNotes] = useState<Note[]>([]);
+    const [isDetailView, setIsDetailView] = useState<boolean>(false);
+    const [currentNote, setCurrentNote] = useState<Note>();
 
     const navigate = useNavigate();
 
@@ -57,7 +61,14 @@ function Dashboard() {
 
             setNotes(data);
         }
-    }
+    };
+
+    const ViewNoteDetail = (noteID: number) => {
+
+        setIsDetailView(true);
+
+        setCurrentNote(notes.find((note: Note) => note.id === noteID));
+    };
 
     return (
         <div className={styles.wrapper}>
@@ -65,62 +76,135 @@ function Dashboard() {
             {/* HEADER */}
             {/* header - should be turned into a component if need be. */}
             <div className={styles.headerContainer}>
-                <img src={logoIcon} alt="logo" />
+                <img src={logoIcon} alt="logo"/>
             </div>
 
             {/* CONTENT */}
             {/* content (middle section) - should be turned into multiple components. */}
             {/* content section will adapt to with responsive design. */}
             <div className={styles.contentContainer}>
-                <h3>All Notes</h3>
-                <ul className={styles.listContainer}>
-                    {notes.map((note: Note) => (
-                        <li key={note.id} className={styles.noteContainer}>
-                            <strong>{note.title}</strong>
-                            <div className={styles.tagContainer}>
-                                <p>Dev</p>
-                                <p>React</p>
+
+                {!isDetailView
+
+                    ? // TRUE
+                    (<>
+                        {/* Content header - this can be turned into a component if need be*/}
+                        <h3>All Notes</h3>
+                        {/* Note List Container - this will turn into its own component if need be.*/}
+                        <ul className={styles.noteListContainer}>
+                            {notes.map((note: Note) => (
+                                <li key={note.id} className={styles.noteContainer}
+                                    onClick={() => ViewNoteDetail(note.id)}>
+                                    <strong>{note.title}</strong>
+                                    <div className={styles.tagContainer}>
+                                        <p>Dev</p>
+                                        <p>React</p>
+                                    </div>
+                                    <p>29 Oct 2024</p>
+                                    <hr/>
+                                </li>
+                            ))}
+                        </ul>
+                    </>)
+
+                    : // ELSE
+                    /* Note Detail Container   */
+                    (<div className={styles.noteDetailContainer}>
+                        {/* Header container for note detail container*/}
+                        <div className={styles.headerContainer}>
+                            <Link to={"/dashboard"} className={styles.backLinkContainer}
+                                  onClick={() => setIsDetailView(false)}>
+                                <img src={leftArrowIcon} alt="Left arrow icon"/>
+                                <p>Go Back</p>
+                            </Link>
+                            <div className={styles.buttonContainer}>
+                                <button>
+                                    <img src={trashIcon} alt="Trash delete icon button"/>
+                                </button>
+                                <button>
+                                    <img src={archiveIcon} alt="Archive icon button"/>
+                                </button>
+                                <button>
+                                    Cancel
+                                </button>
+                                <button className={styles.saveButton}>
+                                    Save Note
+                                </button>
                             </div>
-                            <p>29 Oct 2024</p>
-                            <hr />
-                        </li>
-                    ))}
-                </ul>
+                        </div>
+                        <div className={styles.contentContainer}>
+                            {currentNote
+                                ?
+                                // display note details
+                                (<>
+                                    <div className={styles.headerContainer}>
+                                        <h3 className={styles.noteTitle}>
+                                            {currentNote.title}
+                                        </h3>
+                                        <div className={styles.tagContainer}>
+                                            <div className={styles.titleContainer}>
+                                                <img src={tagIcon} alt="tag icon"/>
+                                                <p>Tags</p>
+                                            </div>
+                                            <div className={styles.listContainer}>
+                                                <p>Dev, React</p>
+                                            </div>
+                                        </div>
+                                        <div className={styles.timestampContainer}>
+                                            <div className={styles.titleContainer}>
+                                                <img src={timeIcon} />
+                                                <p>Last Edited</p>
+                                            </div>
+                                            <p>
+                                                29 Oct 2024
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={styles.textContainer}>
+                                        {currentNote.note}
+                                    </div>
+                                </>)
+
+                                :
+                                // WARNING: SOMETHING WENT WRONG
+                                "ERROR: CURRENT NOTE IS NULL"}
+                        </div>
+                    </div>)
+                }
+
             </div>
 
             {/* FOOTER */}
             {/* footer (mobile menu bar) - should be its own component.*/}
             <div className={styles.footerContainer}>
-                <div className={styles.buttonContainer}>
+                <div className={styles.createButtonContainer}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M12 5a.75.75 0 0 1 .75.75V11H18a.75.75 0 0 1 0 1.5h-5.25v5.25a.75.75 0 0 1-1.5 0V12.5H6A.75.75 0 0 1 6 11h5.25V5.75A.75.75 0 0 1 12 5Z"/>
+                        <path
+                            d="M12 5a.75.75 0 0 1 .75.75V11H18a.75.75 0 0 1 0 1.5h-5.25v5.25a.75.75 0 0 1-1.5 0V12.5H6A.75.75 0 0 1 6 11h5.25V5.75A.75.75 0 0 1 12 5Z"/>
                     </svg>
                 </div>
                 <div className={styles.menuBarContainer}>
                     <Link to={"/Dashboard"}>
-                        <img src={homeIcon} alt="home icon" />
+                        <img src={homeIcon} alt="home icon"/>
                     </Link>
-
                     <Link to={"/Dashboard"}>
-                        <img src={searchIcon} alt="home icon" />
+                        <img src={searchIcon} alt="home icon"/>
                     </Link>
-
                     <Link to={"/Dashboard"}>
-                        <img src={archiveIcon} alt="home icon" />
+                        <img src={archiveIcon} alt="home icon"/>
                     </Link>
-
                     <Link to={"/Dashboard"}>
-                        <img src={tagIcon} alt="home icon" />
+                        <img src={tagIcon} alt="home icon"/>
                     </Link>
-
                     {/* For now the settings button will redirect to logout until settings is created*/}
                     <Link to={"/logout"}>
-                        <img src={settingsIcon} alt="home icon" />
+                        <img src={settingsIcon} alt="home icon"/>
                     </Link>
                 </div>
             </div>
         </div>
-    )
+    );
+
 }
 
 export default Dashboard;
